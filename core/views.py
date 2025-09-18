152d3dad47184a41cd7be9fb8c52956ae0b56c10
huguescodeur <huguescodeur@gmail.com>
2025-09-18 22:30:54 +0000

    Add final coat in validate request in view.py

--- core/views.py
+++ core/views.py
@@ -590,6 +590,9 @@ def validate_request(request, pk):
             purchase_request.rejected_by = request.user
             purchase_request.rejected_at = timezone.now()
             purchase_request.rejected_by_role = user_role
+            
+            if final_cost:
+                        purchase_request.final_cost = final_cost
         else:  
             if user_role == 'mg':
                     purchase_request.status = 'mg_approved'
@@ -599,7 +602,8 @@ def validate_request(request, pk):
                     purchase_request.status = 'accounting_reviewed'
                     purchase_request.budget_available = budget_available
                     purchase_request.accounting_validated_by = request.user  
-                    purchase_request.accounting_validated_at = timezone.now()  
+                    purchase_request.accounting_validated_at = timezone.now()
+                      
                     if final_cost:
                         purchase_request.final_cost = final_cost
             elif user_role == 'director':
